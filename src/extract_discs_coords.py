97207669c23e438106358f43be4683e37fb2cb66
Nathan Molinier <nathan.molinier@gmail.com>
2023-03-15 10:28:11 -0400

    Add missing parser info

--- src/extract_discs_coords.py
+++ src/extract_discs_coords.py
@@ -399,12 +399,21 @@ if __name__=='__main__':
     parser.add_argument('-c', '--modality', type=str, metavar='N', required=True,
                         help='Data modality')
     parser.add_argument('-txt', '--out_txt_file', default='prepared_data/discs_coords.txt', type=str, metavar='N',
-                        help='Generated txt file')                                                                                                
+                        help='Generated txt file')
+    
+    parser.add_argument('--att', default= True, type=bool,
+                        help=' Use attention mechanism') 
+    parser.add_argument('-s', '--stacks', default=2, type=int, metavar='N',
+                        help='Number of hourglasses to stack')
+    parser.add_argument('-b', '--blocks', default=1, type=int, metavar='N',
+                        help='Number of residual modules at each location in the hourglass')
+    parser.add_argument('--njoints', default=11, type=int,
+                        help='Number of joints')                                                                                               
     
     if not os.path.exists(parser.parse_args().out_txt_file):
         print("Creating txt file:", parser.parse_args().out_txt_file)
         with open(parser.parse_args().out_txt_file,"w") as f:
             f.write("subject_name contrast num_disc sct_discs_coords hourglass_coords gt_coords\n")
 
-    test_sct_label_vertebrae(parser.parse_args())
+    # test_sct_label_vertebrae(parser.parse_args())
     test_hourglass(parser.parse_args())
